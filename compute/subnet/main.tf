270ee1b1993dc13a74fbef18b2a8dda6b356e595
Denis Craig <admin@deniscraig.com>
2019-04-30 23:47:33 +0100

    Fix subnetwork name var

--- compute/subnet/main.tf
+++ compute/subnet/main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_subnetwork" "subnet" {
   count         = length(var.cidr_blocks)
-  name          = "${"${var.name}-${replace(replace(element(var.cidr_blocks, count.index), ".", "-"), "/", "-")}"}"
+  name          = "${var.name}-${replace(replace(element(var.cidr_blocks, count.index), ".", "-"), "/", "-")}"
   network       = var.vpc_id
   ip_cidr_range = var.cidr_blocks[count.index]
 }
